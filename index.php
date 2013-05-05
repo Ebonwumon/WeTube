3eb7c43bf17d1b25f7643a3dde2ad0d2e72820f6
Troy Pavlek <ebonwumon@depotwarehouse.net>
2013-05-04 23:51:29 -0600

    index

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: ebon
+ * Date: 04/05/13
+ * Time: 23:51
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "hello world";
+
+?>
