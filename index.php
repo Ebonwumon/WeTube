243872a9eee63c523abff9ffdb03f933a9dde778
Troy Pavlek <ebonwumon@depotwarehouse.net>
2013-05-05 10:06:16 +0200

    js inputted

--- index.php
+++ index.php
@@ -8,6 +8,23 @@ require_once('db.php');
  * To change this template use File | Settings | File Templates.
  */
 $db = new DB();
-print_r($db->playNext(1));
 
 ?>
+<script type="text/javascript" src="swfobject/swfobject.js"></script>    
+  <div id="ytapiplayer">
+        You need Flash player 8+ and JavaScript enabled to view this video.
+          </div>
+
+  <script type="text/javascript">
+
+    var params = { allowScriptAccess: "always" };
+    var atts = { id: "myytplayer" };
+    swfobject.embedSWF("http://www.youtube.com/apiplayer?enablejsapi=1&version=3",
+      "ytapiplayer", "960", "540", "8", null, null, params, atts);
+
+    function onYouTubePlayerReady(playerid) {
+      var player = document.getElementById("myytplayer");
+      player.loadVideoById("E5mwp264g-Q");
+      player.playVideo(); 
+    }
+      </script>
